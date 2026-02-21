67609fde8728acf2cf140b572a2667461a8f2806
Maksym Senko <maksskazka1997@gmail.com>
2026-02-21 21:39:00 +0100

    Correcrt-task-6

--- src/task-6.ts
+++ src/task-6.ts
@@ -6,6 +6,6 @@ getFirstElement<number>([1, 2, 3]);           // 1
 getFirstElement<string>(["a", "b", "c"]);     // "a"
 getFirstElement<boolean>([true, false, true]); // true
 
-console.log(getFirstElement([1, 2, 3]));
-console.log(getFirstElement(["a", "b", "c"]))
-console.log(getFirstElement([true, false, true]))
+// console.log(getFirstElement([1, 2, 3]));
+// console.log(getFirstElement(["a", "b", "c"]))
+// console.log(getFirstElement([true, false, true]))
